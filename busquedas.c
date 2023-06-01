4959a84859aabd3c2ba18cb45625dfe35bebe886
Y0SDan <yosd4n@gmail.com>
2023-06-01 07:42:02 -0600

    le subi al rand

--- busquedas.c
+++ busquedas.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <stdbool.h>
 
-#define MAX 15
+#define MAX 120
 
 void breadth_first_search(int g[MAX][MAX], int n, int start, bool visited[MAX], int values[MAX], int search_value, bool *found) {
     int queue[MAX], front = 0, rear = 0;
@@ -53,27 +53,31 @@ int main() {
     }
     printf("\nValores aleatorios asignados a cada nodo:\n");
     for (i = 0; i < n; i++) {
-        values[i] = rand() % 100;
+        values[i] = rand() % 150;
         printf("Nodo %d: %d\n", i, values[i]);
     }
     int search_value, search_method;
-    printf("\nIngresa un número para buscar en el grafo: ");
-    scanf("%d", &search_value);
-    printf("\nSelecciona el método de búsqueda:\n1. Búsqueda en anchura (BFS)\n2. Búsqueda en profundidad (DFS)\n");
-    scanf("%d", &search_method);
-    clock_t start_time, end_time;
-    double time_taken;
-    start_time = clock();
-    if (search_method == 1)
-        breadth_first_search(g, n, 0, visited, values, search_value, &found);
-    else
-        depth_first_search(g, n, 0, visited, values, search_value, &found);
-    end_time = clock();
-    time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-    if (found)
-        printf("\nEl número %d está en el grafo.\n", search_value);
-    else
-        printf("\nEl número %d no está en el grafo.\n", search_value);
-    printf("Tiempo que tardó el algoritmo de búsqueda: %f segundos\n", time_taken);
+    while (true) {
+        printf("\nIngresa un número para buscar en el grafo (ingresa -1 para salir): ");
+        scanf("%d", &search_value);
+        if (search_value == -1)
+            break;
+        printf("\nSelecciona el método de búsqueda:\n1. Búsqueda en anchura (BFS)\n2. Búsqueda en profundidad (DFS)\n");
+        scanf("%d", &search_method);
+        clock_t start_time, end_time;
+        double time_taken;
+        start_time = clock();
+        if (search_method == 1)
+            breadth_first_search(g, n, 0, visited, values, search_value, &found);
+        else
+            depth_first_search(g, n, 0, visited, values, search_value, &found);
+        end_time = clock();
+        time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+        if (found)
+            printf("\nEl número %d está en el grafo.\n", search_value);
+        else
+            printf("\nEl número %d no está en el grafo.\n", search_value);
+        printf("Tiempo que tardó el algoritmo de búsqueda: %f segundos\n", time_taken);
+    }
     return 0;
 }
